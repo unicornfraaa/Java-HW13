5ad4b445c12470083c314059c66e6785f9be7693
Ksenia <ksuskaxD@gmail.com>
2022-07-09 20:38:14 +0300

    added RepositoryTest

---
+++ src/test/java/RepositoryTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class RepositoryTest {
+    Repository repo = new Repository();
+    ProductManager manager = new ProductManager(repo);
+
+    Book book1 = new Book(1, "first magazine", 1000, "Petya");
+    Book book2 = new Book(2, "second magazine", 2000, "Olya");
+    Book book3 = new Book(3, "third", 3000, "Vasya");
+
+    Smartphone phone1 = new Smartphone(11, "S1 phone", 10000, "Sumsung");
+    Smartphone phone2 = new Smartphone(22, "S2 phone", 20000, "Xiaomi");
+    Smartphone phone3 = new Smartphone(33, "S3", 30000, "Apple");
+
+    @Test
+    public void shouldAdded() {
+        repo.add(book1);
+        repo.add(book2);
+        repo.add(phone1);
+
+        Product[] actual = repo.findAll();
+        Product[] expected = {book1, book2, phone1};
+
+        assertArrayEquals(actual,expected);
+    }
+}
